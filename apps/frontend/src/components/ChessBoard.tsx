bd436e10476bab366dde7bd5f92f93b3e69bcb6c
Resham <reshamupadhyay48@gmail.com>
2025-08-06 21:52:38 +0530

    chessboard

--- apps/frontend/src/components/ChessBoard.tsx
+++ apps/frontend/src/components/ChessBoard.tsx
@@ -1,11 +1,16 @@
-import type { Chess, Color, PieceSymbol, Square } from "chess.js";
-import { memo, useEffect, useState } from "react";
+import type { Chess, Color, Move, PieceSymbol, Square } from "chess.js";
+import { memo, useEffect, useState, type MouseEvent } from "react";
 import MoveSound from "/move.wav";
 import CaptureSound from "/capture.wav";
 import { useChessBoardStore } from "@repo/store/chessBoardStore";
 
 import ReactConfetti from "react-confetti";
 import NumberNotation from "./chess-board/NumberNotation";
+import { MOVE } from "../pages/Game";
+import { drawArrow } from "../utils/canvas";
+import ChessSquare from "./chess-board/ChessSquare";
+import LetterNotation from "./chess-board/LetterNotation";
+import LegalMoveIndicator from "./chess-board/LegalMoveIndicator";
 
 export function isPromoting(chess: Chess, from: Square, to: Square) {
   if (!from) {
@@ -33,9 +38,9 @@ export function isPromoting(chess: Chess, from: Square, to: Square) {
 }
 
 interface ChessBoardProps {
-  gameId: string;
-  started: boolean;
-  myColor: Color;
+  gameId?: string;
+  started?: boolean;
+  myColor?: Color;
   chess: Chess;
   board: ({
     square: Square;
@@ -54,102 +59,340 @@ interface ChessBoardProps {
   >;
 }
 
-const ChessBoard = ({
-  gameId,
-  started,
-  myColor,
-  chess,
-  board,
-  socket,
-  setBoard,
-}: ChessBoardProps) => {
-  const isFlipped = useChessBoardStore((s) => s.isBoardFlipped);
-  const setIsFlipped = useChessBoardStore((s) => s.setIsBoardFlipped);
-  const move = useChessBoardStore((s) => s.moves);
-  const setMoves = useChessBoardStore((s) => s.setMoves);
-  const addMoves = useChessBoardStore((s) => s.addMove);
-  const userSelectedMoveIndex = useChessBoardStore(
-    (s) => s.userSelectedMoveIndex
-  );
-  const setUserSelectedMoveIndex = useChessBoardStore(
-    (s) => s.setUserSelectedMoveIndex
-  );
-  const [gameOver, setGameOver] = useState(false);
-  const [from, setFrom] = useState<null | Square>(null);
-  const [lastMove, setLastMove] = useState<{ from: string; to: string } | null>(
-    null
-  );
-  const isMyTurn = myColor === chess.turn();
-  const [legalMove, setLegalMove] = useState<string[]>([]);
-  const labels = ["a", "b", "c", "d", "e", "f", "g", "h"];
-  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
-  const boxSize = 80;
-  const [gameOver, setGameOver] = useState(false);
-  const moveAudio = new Audio(MoveSound);
-  const captureAudio = new Audio(CaptureSound);
+export const ChessBoard = memo(
+  ({
+    gameId,
+    started,
+    myColor,
+    chess,
+    board,
+    socket,
+    setBoard,
+  }: ChessBoardProps) => {
+    const isFlipped = useChessBoardStore((s) => s.isBoardFlipped);
+    const setIsFlipped = useChessBoardStore((s) => s.setIsBoardFlipped);
+    const moves = useChessBoardStore((s) => s.moves);
+    const setMoves = useChessBoardStore((s) => s.setMoves);
+    const addMoves = useChessBoardStore((s) => s.addMove);
+    const userSelectedMoveIndex = useChessBoardStore(
+      (s) => s.userSelectedMoveIndex
+    );
+    const setUserSelectedMoveIndex = useChessBoardStore(
+      (s) => s.setUserSelectedMoveIndex
+    );
+    const [from, setFrom] = useState<null | Square>(null);
+    const [lastMove, setLastMove] = useState<{
+      from: string;
+      to: string;
+    } | null>(null);
+    const isMyTurn = myColor === chess.turn();
+    const [legalMoves, setLegalMoves] = useState<string[]>([]);
+    const [rightClickedSquares, setRightClickedSquares] = useState<string[]>(
+      []
+    );
+    const labels = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
+    const [arrowStart, setArrowStart] = useState<string | null>(null);
+    const boxSize = 80;
+    const [gameOver, setGameOver] = useState(false);
+    const moveAudio = new Audio(MoveSound);
+    const captureAudio = new Audio(CaptureSound);
 
-  console.log("chessboard reloaded");
-
-  useEffect(() => {
-    if (myColor === "b") {
-      setIsFlipped(true);
+    function handleRightClick(squareRep: string) {
+      if (rightClickedSquares.includes(squareRep)) {
+        setRightClickedSquares((prev) => prev.filter((sq) => sq !== squareRep));
+      } else {
+        setRightClickedSquares((prev) => [...prev, squareRep]);
+      }
     }
-  }, [myColor]);
-  
 
-  return (
-    <>
-      {gameOver && <ReactConfetti />}
-      <div className="flex relative">
-        <div className="text-white rounded-md overflow-hidden">
-          {(isFlipped ? board.slice().reverse() : board).map((row,i) => {
-            i = isFlipped ? i + 1 : 8 - i;
-            return(
-              <div key={i} className="flex relative">
-                <NumberNotation isMainBoxColor={isFlipped ? i % 2 !== 0 : i % 2 === 0} label={i.toString()}/>
-                {(isFlipped ? row.slice().reverse() : row).map((square, j) => {
-                   j = isFlipped ? 7 - (j % 8) : j % 8;
-                   const isMainBoxColor = (i + j) % 2 !== 0;
-                    const isPiece: boolean = !!square;
-                    const squareRepresentation = (String.fromCharCode(97 + j) + '' + i) as Square;
-                    const isHighlightedSquare =
-                      from === squareRepresentation ||
-                      squareRepresentation === lastMove?.from ||
-                      squareRepresentation === lastMove?.to;
-                    const isRightClickedSquare = rightClickedSquares.includes(squareRepresentation);
+    function handleDrawArrow(squareRep: string) {
+      if (arrowStart) {
+        const stoppedAtSquare = squareRep;
+        if (canvas) {
+          const ctx = canvas.getContext("2d");
+          if (ctx) {
+            drawArrow({
+              ctx,
+              start: arrowStart,
+              end: stoppedAtSquare,
+              isFlipped,
+              squareSize: boxSize,
+            });
+          }
+        }
+        setArrowStart(null);
+      }
+    }
 
-                    const piece = square && square.type;
-                    const isKingInCheckSquare = piece === 'k' && square?.color === chess.turn() && chess.inCheck();
+    const clearCanvas = () => {
+      setRightClickedSquares([]);
+      if (canvas) {
+        const ctx = canvas.getContext("2d");
+        ctx?.clearRect(0, 0, canvas.width, canvas.height);
+      }
+    };
 
-                    return( 
-                      <div onClick={()=>{}}>
+    function handleMouseDown(e: MouseEvent<HTMLDivElement>, squareRep: string) {
+      e.preventDefault();
+      if (e.button === 2) {
+        setArrowStart(squareRep);
+      }
+    }
 
-                      </div>
-                    )
-                })}
-              </div>
-            )
-          })}
+    function handleMouseUp(e: MouseEvent<HTMLDivElement>, squareRep: string) {
+      e.preventDefault();
+      if (!started) {
+        return;
+      }
+
+      if (e.button === 2) {
+        if (arrowStart === squareRep) {
+          handleRightClick(squareRep);
+        } else {
+          handleDrawArrow(squareRep);
+        }
+      } else {
+        clearCanvas();
+      }
+    }
+
+    useEffect(() => {
+      if (myColor === "b") {
+        setIsFlipped(true);
+      }
+    }, [myColor]);
+
+    useEffect(() => {
+      clearCanvas();
+      const lMove = moves.at(-1);
+      if (lMove) {
+        setLastMove({
+          from: lMove.from,
+          to: lMove.to,
+        });
+      } else {
+        setLastMove(null);
+      }
+    }, [moves]);
+
+    useEffect(() => {
+      if (userSelectedMoveIndex !== null) {
+        const move = moves[userSelectedMoveIndex];
+        setLastMove({
+          from: move.from,
+          to: move.to,
+        });
+        chess.load(move.after);
+        setBoard(chess.board());
+        return;
+      }
+    }, [userSelectedMoveIndex]);
+
+    useEffect(() => {
+      if (userSelectedMoveIndex !== null) {
+        chess.reset();
+        moves.forEach((move) => {
+          chess.move({ from: move.from, to: move.to });
+        });
+        setBoard(chess.board());
+        setUserSelectedMoveIndex(null);
+      } else {
+        setBoard(chess.board());
+      }
+    }, [moves]);
+
+    return (
+      <>
+        {gameOver && <ReactConfetti />}
+        <div className="flex relative">
+          <div className="text-white rounded-md overflow-hidden">
+            {(isFlipped ? board.slice().reverse() : board).map((row, i) => {
+              i = isFlipped ? i + 1 : 8 - i;
+              return (
+                <div key={i} className="flex relative">
+                  <NumberNotation
+                    isMainBoxColor={isFlipped ? i % 2 !== 0 : i % 2 === 0}
+                    label={i.toString()}
+                  />
+                  {(isFlipped ? row.slice().reverse() : row).map(
+                    (square, j) => {
+                      j = isFlipped ? 7 - (j % 8) : j % 8;
+                      const isMainBoxColor = (i + j) % 2 !== 0;
+                      const isPiece: boolean = !!square;
+                      const squareRepresentation = (String.fromCharCode(
+                        97 + j
+                      ) +
+                        "" +
+                        i) as Square;
+                      const isHighlightedSquare =
+                        from === squareRepresentation ||
+                        squareRepresentation === lastMove?.from ||
+                        squareRepresentation === lastMove?.to;
+                      const isRightClickedSquare =
+                        rightClickedSquares.includes(squareRepresentation);
+
+                      const piece = square && square.type;
+                      const isKingInCheckSquare =
+                        piece === "k" &&
+                        square?.color === chess.turn() &&
+                        chess.inCheck();
+
+                      return (
+                        <div
+                          onClick={() => {
+                            if (!started) {
+                              return;
+                            }
+                            if (userSelectedMoveIndex !== null) {
+                              chess.reset();
+                              moves.forEach((move) => {
+                                chess.move({ from: move.from, to: move.to });
+                              });
+                              setBoard(chess.board());
+                              setUserSelectedMoveIndex(null);
+                              return;
+                            }
+                            if (!from && square?.color !== chess.turn()) return;
+                            if (!isMyTurn) return;
+                            if (from != squareRepresentation) {
+                              setFrom(squareRepresentation);
+                              if (isPiece) {
+                                setLegalMoves(
+                                  chess
+                                    .moves({
+                                      verbose: true,
+                                      square: square?.square,
+                                    })
+                                    .map((move) => move.to)
+                                );
+                              }
+                            } else {
+                              setFrom(null);
+                            }
+                            if (!isPiece) {
+                              setLegalMoves([]);
+                            }
+
+                            if (!from) {
+                              setFrom(squareRepresentation);
+                              setLegalMoves(
+                                chess
+                                  .moves({
+                                    verbose: true,
+                                    square: square?.square,
+                                  })
+                                  .map((move) => move.to)
+                              );
+                            } else {
+                              try {
+                                let moveResult: Move;
+                                if (
+                                  isPromoting(chess, from, squareRepresentation)
+                                ) {
+                                  moveResult = chess.move({
+                                    from,
+                                    to: squareRepresentation,
+                                    promotion: "q",
+                                  });
+                                } else {
+                                  moveResult = chess.move({
+                                    from,
+                                    to: squareRepresentation,
+                                  });
+                                }
+                                if (moveResult) {
+                                  moveAudio.play();
+
+                                  if (moveResult?.captured) {
+                                    captureAudio.play();
+                                  }
+                                  addMoves(moveResult);
+                                  setFrom(null);
+                                  setLegalMoves([]);
+                                  if (moveResult.san.includes("#")) {
+                                    setGameOver(true);
+                                  }
+                                  socket.send(
+                                    JSON.stringify({
+                                      type: MOVE,
+                                      payload: {
+                                        gameId,
+                                        move: moveResult,
+                                      },
+                                    })
+                                  );
+                                }
+                              } catch (e) {
+                                console.log("e", e);
+                              }
+                            }
+                          }}
+                          style={{
+                            width: boxSize,
+                            height: boxSize,
+                          }}
+                          key={j}
+                          className={`${isRightClickedSquare ? (isMainBoxColor ? "bg-[#CF664E]" : "bg-[#E87764]") : isKingInCheckSquare ? "bg-[#FF6347]" : isHighlightedSquare ? `${isMainBoxColor ? "bg-[#BBCB45]" : "bg-[#F4F687]"}` : isMainBoxColor ? "bg-boardDark" : "bg-boardLight"} ${""}`}
+                          onContextMenu={(e) => {
+                            e.preventDefault();
+                          }}
+                          onMouseDown={(e) => {
+                            handleMouseDown(e, squareRepresentation);
+                          }}
+                          onMouseUp={(e) => {
+                            handleMouseUp(e, squareRepresentation);
+                          }}
+                        >
+                          <div className="w-full justify-center flex h-full relative">
+                            {square && <ChessSquare square={square} />}
+                            {isFlipped
+                              ? i === 8 && (
+                                  <LetterNotation
+                                    label={labels[j]}
+                                    isMainBoxColor={j % 2 === 0}
+                                  />
+                                )
+                              : i === 1 && (
+                                  <LetterNotation
+                                    label={labels[j]}
+                                    isMainBoxColor={j % 2 !== 0}
+                                  />
+                                )}
+                            {!!from &&
+                              legalMoves.includes(squareRepresentation) && (
+                                <LegalMoveIndicator
+                                  isMainBoxColor={isMainBoxColor}
+                                  isPiece={!!square?.type}
+                                />
+                              )}
+                          </div>
+                        </div>
+                      );
+                    }
+                  )}
+                </div>
+              );
+            })}
+          </div>
+          <canvas
+            ref={(ref) => setCanvas(ref)}
+            width={boxSize * 8}
+            height={boxSize * 8}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              pointerEvents: "none",
+            }}
+            onContextMenu={(e) => e.preventDefault()}
+            onMouseDown={(e) => {
+              e.preventDefault();
+            }}
+            onMouseUp={(e) => e.preventDefault()}
+          ></canvas>
         </div>
-        <canvas
-          ref={(ref) => setCanvas(ref)}
-          width={boxSize * 8}
-          height={boxSize * 8}
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            pointerEvents: "none",
-          }}
-          onContextMenu={(e) => e.preventDefault()}
-          onMouseDown={(e) => {
-            e.preventDefault();
-          }}
-          onMouseUp={(e) => e.preventDefault()}
-        ></canvas>
-      </div>
-    </>
-  );
-};
-
-export default memo(ChessBoard);
+      </>
+    );
+  }
+);
